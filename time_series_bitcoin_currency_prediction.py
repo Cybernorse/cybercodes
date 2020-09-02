c817ed21b58622070a368cec3942f58e185517c2
WaqarAhmad <42264502+Cybernorse@users.noreply.github.com>
2020-09-02 13:43:42 +0500

    Add files via upload

--- time_series_bitcoin_currency_prediction.py
+++ time_series_bitcoin_currency_prediction.py
@@ -1,32 +1,46 @@
-from fbprophet import Prophet
-import pandas as pd
-import numpy as np
+# from fbprophet import Prophet
+# import pandas as pd
+# import numpy as np
+# from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error,roc_auc_score,auc,accuracy_score,matthews_corrcoef
+# class bitcoin_predictions:
+#     def __init__(self):
+#         df=pd.read_excel("/home/bigpenguin/Downloads/bitcoin_db.xlsx")
+#         # df.drop('Total Volume',axis=1,inplace=True)
+#         df.drop('Open',axis=1,inplace=True)
+#         df.drop('High',axis=1,inplace=True)
+#         df.drop('Low',axis=1,inplace=True)
+#         df.drop('Return',axis=1,inplace=True)
+#         df.drop('Vol.',axis=1,inplace=True)
+#         df.drop('Change %',axis=1,inplace=True)
+#         # print(df.head())
+        
+#         df.rename(columns={'Date': 'ds', 'Price': 'y'},inplace=True)
+        
+#         df['y']=np.log(df['y'])
 
-class bitcoin_predictions:
-    def __init__(self):
-        df=pd.read_csv("/home/bigpenguin/Downloads/BTC-USD.csv")
-        df.drop('Open',axis=1,inplace=True)
-        df.drop('High',axis=1,inplace=True)
-        df.drop('Low',axis=1,inplace=True)
-        df.drop('Adj Close',axis=1,inplace=True)
-        df.drop('Volume',axis=1,inplace=True)
-        # print(df.head())
-        df.rename(columns={'Date': 'ds', 'Close': 'y'},inplace=True)
+#         m=Prophet( daily_seasonality=True,interval_width=0.95)
+#         m.fit(df)
 
-        df['y']=np.log(df['y'])
+#         future=m.make_future_dataframe(periods=720,include_history=True)
+#         future.tail()
 
-        m=Prophet(daily_seasonality=True,interval_width=0.95)
-        m.fit(df)
+#         pred=m.predict(future)
+    
+#         pred[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
+        
+#         print(len(pred['yhat'][:3642]))
+        
 
-        future=m.make_future_dataframe(periods=60,freq='D')
-        future.tail()
-
-        pred=m.predict(future)
-        pred[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
-
-        fig=m.plot(pred)
-        fig.savefig('/home/bigpenguin/01_fbprophet_getting_started-02.png')
+#         # print((sum(pred['yhat'])-sum(df['y']))/sum(df['y']))
+#         # fig=m.plot(pred)
+#         # fig=m.plot_components(pred)
+#         # fig.savefig('/home/bigpenguin/01_fbprophet_getting_started-03.png')
         
         
-if __name__=="__main__":
-    predict_obj=bitcoin_predictions()
+# if __name__=="__main__":
+#     predict_obj=bitcoin_predictions()
+
+
+
+for i in app:
+    print(i)

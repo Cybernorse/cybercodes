857b7d4d6987ea1cd5c2a324a8350ec108c39595
WaqarAhmad <42264502+Cybernorse@users.noreply.github.com>
2020-09-12 22:56:35 +0500

    Update channel_fb.py

--- channel_info/spiders/channel_fb.py
+++ channel_info/spiders/channel_fb.py
@@ -18,10 +18,13 @@ class channel_about_info(scrapy.Spider):
         super(channel_about_info, self).__init__(*args, **kwargs)
         self.count=0
         self.nasty_duplicates=[]
+        
+#       read youtube channel names 
         with open('/home/linuxw/Downloads/channel_names.csv','r') as f:
             self.channels_name=[i for i in f.readlines()]
 
     def start_requests(self):
+#       send request to the google search engine
         start_urls=['https://www.google.com/']
         yield SeleniumRequest(
             url=start_urls[0],
@@ -36,7 +39,8 @@ class channel_about_info(scrapy.Spider):
         channel_link=[]
 
         print("############",self.channels_name[self.count],self.count,"############")
-        # self.channels_name[self.count]
+        
+#       create a query and feed to the google search engine and press enter
         query=self.channels_name[self.count]+" "+"facebook"
         search=self.driver.find_element_by_name('q')
         search.send_keys(query)
@@ -47,13 +51,15 @@ class channel_about_info(scrapy.Spider):
             WebDriverWait(self.driver,20).until(EC.presence_of_element_located((By.ID,"pnnext")))
         except:
             time.sleep(10)
-
+            
+#       get the links of all results select the ones with facebook top domain 
         results=Selector(text=self.driver.page_source).response.xpath('//div[@class="r"] /a/@href').getall()
         print(results)
         for i in results:
             if "www.facebook.com"==urlparse(i).netloc.lower():
                 channel_link.append(i)
-        
+                
+#       if no links with facebook url are found callback parse method 
         if len(channel_link)==0:
             yield SeleniumRequest(
                 url='https://www.google.com/',
@@ -64,7 +70,8 @@ class channel_about_info(scrapy.Spider):
             )
 
         self.driver.save_screenshot('image1.png')
-
+        
+#       if a duplicate url is found callback parse method 
         if channel_link[0] in self.nasty_duplicates:
             yield SeleniumRequest(
                 url='https://www.google.com/',
@@ -75,7 +82,7 @@ class channel_about_info(scrapy.Spider):
             )
 
         self.nasty_duplicates.append(channel_link[0])
-
+#       send request to the facebook page to fetch the page code
         try:
             yield SeleniumRequest(
                 url=channel_link[0],
@@ -98,6 +105,8 @@ class channel_about_info(scrapy.Spider):
         self.channel_type=[]
         self.pc=[]
         self.l_f=[]
+        
+#       getting some data from the home page 
         if response.xpath('//div[@class="_4bl9"]/ div//text()').getall()[0:2]:
             self.l_f=response.xpath('//div[@class="_4bl9"]/ div//text()').getall()[0:2]
 
@@ -109,17 +118,12 @@ class channel_about_info(scrapy.Spider):
             self.channel_type=[self.path1[len(self.path1)-1]]
 
         try:
+#           put '/' in the url path where there is no '/' at the end and create a about page url
             set_url=''
             set_url=self.driver.current_url
             if not str(set_url).endswith('/'):
                 set_url+='/'
             self.aboutc=urlparse(self.driver.current_url).scheme+"://"+urlparse(self.driver.current_url).netloc+"/pg"+urlparse(self.driver.current_url).path+"about/?ref=page_internal"
-            # get_aboutc=self.driver.find_element_by_xpath('//div[@class="_2yaa"]')
-            # get_aboutc.click()
-            # try:
-            #     WebDriverWait(self.driver,20).until(EC.presence_of_element_located((By.CLASS_NAME,"_50f7")))
-            # except:
-            #     time.sleep(10)
 
         except Exception as e:
             print(e)
@@ -133,7 +137,8 @@ class channel_about_info(scrapy.Spider):
 
         self.driver.save_screenshot('image2.png')
         print(self.aboutc)
-
+        
+#       send request to about page 
         yield SeleniumRequest(
             url=self.aboutc,
             callback=self.get_about,
@@ -144,6 +149,7 @@ class channel_about_info(scrapy.Spider):
     
     def get_about(self,response):
         ids=[]
+#       get all the information found on the about page 
         description=response.xpath('//div[@class="_3-8w"]/text()').getall()+response.xpath('//div[@class="text_exposed_root"]/text()').getall()+response.xpath('//span[@class="text_exposed_show"]/text()').getall()
         for i in response.xpath('//meta/@content').getall():
             if "?id" in i :
@@ -164,10 +170,11 @@ class channel_about_info(scrapy.Spider):
             'description':[' '.join(description)],
             
         }
-
+#       check if the last channel name has come if yes close the spider 
         if self.channels_name[len(self.channels_name)-1] == self.channels_name[self.count]:
             raise CloseSpider('{!}{!}{!}{!}	Extraction Complete	{!}{!}{!}{!}')
-
+            
+#       call parse method to repeat the process for the next channel name
         yield SeleniumRequest(
             url='https://www.google.com/',
             callback=self.parse,
